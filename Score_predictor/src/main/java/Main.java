97e179d55be3b3b82bbab7392aa1f3070d9eae2d
Melweder <lukasz.pawel.wach@gmail.com>
2020-08-18 17:58:02 +0200

    -m update 1.04

--- Score_predictor/src/main/java/Main.java
+++ Score_predictor/src/main/java/Main.java
@@ -1,20 +1,13 @@
-import DataProcessing.DataRetriever;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.Map;
-
 public class Main extends Application {
 
     public static void main(String[] args) {
-        /*DataProcessing.DataRetriever dataRetriever = new DataProcessing.DataRetriever();
-        ArrayList<Map<String,String>> list = dataRetriever.getMatchStatistics("https://www.football-data.co.uk/mmz4281/1920/E0.csv");
-        DataProcessing.DataProcessor dataProcessor = new DataProcessing.DataProcessor();
-        System.out.println(dataProcessor.predictScore(list,"Man United", "Liverpool"));*/
         launch(args);
     }
 
